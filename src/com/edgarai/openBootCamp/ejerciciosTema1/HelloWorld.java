a50c6b9bf21c7eaaff46d0f548dc83da18962480
Edgar Albalate <dev.gardo@gmail.com>
2022-07-04 06:53:17 +0200

    Adds javadoc to main function

--- src/com/edgarai/openBootCamp/ejerciciosTema1/HelloWorld.java
+++ src/com/edgarai/openBootCamp/ejerciciosTema1/HelloWorld.java
@@ -1,6 +1,12 @@
 package com.edgarai.openBootCamp.ejerciciosTema1;
 
 public class HelloWorld {
+
+    /**
+     * Main function, prints "Hello World"
+     * @param args the command line arguments
+     * @author Edgar Albalate
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
